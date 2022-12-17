a197579cbc4c47f579bfd325d7f343fe6a545b83
awbwd4 <awbwd4@gmail.com>
2022-12-18 00:59:51 +0900

    20221218

--- app/main.py
+++ app/main.py
@@ -8,32 +8,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-print(BASE_DIR)
-
 
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory=BASE_DIR/"templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request:Request):
-    # print(request["headers"])
     return templates.TemplateResponse("./index.html", {"request":request, "title":"콜렉터 북북이"})
-
 
 @app.get("/search", response_class=HTMLResponse)
 async def search(request:Request, q:str):
-    print(q)
-    # print(request["headers"])
     return templates.TemplateResponse("./index.html", {"request":request, "title":"콜렉터 북북이", "keyword":q})
-
-
-
-
-# @app.get("/items/{id}", response_class=HTMLResponse)
-# async def read_time(request:Request, id:str,  data:Optional[str]=None):
-#     # print(request["headers"])
-#     return templates.TemplateResponse("./item.html", {"request":request, "id":id, "data":data})
-
-
